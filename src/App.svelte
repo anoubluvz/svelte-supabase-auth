d6c7008ecbfcf2cfd214e8d02e9eef50ede5d7ad
unknown <syntomy>
2021-08-16 18:55:36 +0200

    Add session store

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,53 @@
 <script>
-	
+	import { supabase } from "./supabaseClient";
+	import { user } from "./sessionStore";
+	import Login from "./Login.svelte";
+
+	let loginPopup = false;
+	let signupPopup = false;
+
+	user.set(supabase.auth.user);
+
+	supabase.auth.onAuthStateChange((__, session) => {
+		user.set(session);
+	});
+
+	const popupHandler = (popupType) => {
+		switch(popupType)
+		{
+			case "login":
+				loginPopup = true;
+				break;
+			case "signup":
+				signupPopup = true;
+				break;
+			default:
+				break;
+		}
+	}
+
+	const switchHandler = (e) => {
+		switch(e.detail)
+		{
+			case "login":
+				loginPopup = true;
+				signupPopup = false;
+				break;
+			case "signup":
+				loginPopup = false;
+				signupPopup = true;
+				break;
+			default:
+				break;
+		}
+	}
 </script>
 
 <main>
-	<h1>SimpleAuth Website</h1>
+    <h1>Simple authentication app</h1>
+	{#if loginPopup == false}
+	<button on:click={popupHandler("login")}>Login</button>
+	{:else}
+	<Login on:switch={switchHandler} />
+	{/if}
 </main>
---
+++ src/Login.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+
+    let dispatch = createEventDispatcher();
+
+    let email;
+    let password;
+    
+    const handleLogin = () => {
+        
+    }
+
+    const handleSignup = () => dispatch("switch", "signup");
+</script>
+
+<form class="loginForm" on:click|preventDefault={handleLogin}>
+    <input type="email" bind:value={email}>
+    <input type="password" bind:value={password}>
+    <a on:click={handleSignup} href="/">Don't have an account?</a>
+</form>
